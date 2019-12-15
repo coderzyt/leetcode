1851df5e56cb68568475bbf8afa681ef5114c4f7
zyt528 <zyt528@outlook.com>
2019-12-15 16:50:03 +0800

    fix

--- homework/Solution.py
+++ homework/Solution.py
@@ -5,7 +5,29 @@ from python.ListNode import ListNode
 class Solution(object):
 
     def trap(self, height: list) -> int:
-        stack = stack()
+        if height is None:
+            return 0
+        length = len(height)
+        if length < 2:
+            return 0
+        L = 0
+        R = length - 1
+        area = 0
+        leftMax, rightMax = 0, 0
+        while L < R:
+            if height[L] < height[R]:
+                if height[L] >= leftMax:
+                    leftMax = height[L]
+                else:
+                    area = area + (leftMax - height[L])
+                L = L + 1
+            else:
+                if height[R] >= rightMax:
+                    rightMax = height[R]
+                else:
+                    area = area + (rightMax - height[R])
+                R = R - 1
+        return area
 
 
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
