c1416785f69a3ce20174e1bf1ceea19dce664c1a
yuntzhao <yuntzhao@ebay.com>
2020-01-10 17:59:37 +0800

    add file

--- CodingInterviewGuide/IsRotation.py
+++ CodingInterviewGuide/IsRotation.py
@@ -1,5 +1,18 @@
 class Solution(object):
 
+    def isRotation(self, str1: str, str2: str) -> bool:
+        if not str1 or not str2:
+            return False
+        if len(str1) != len(str2):
+            return False
+
+        newStr = str2 + str2
+        if newStr.__contains__(str1):
+            return True
+        else:
+            return False
 
 
 if __name__ == '__main__':
+    solution = Solution()
+    print(solution.isRotation('12345', '23451'))
