3d25a8bf9c01ff4b87a6c781a5cee3b3b32b5a78
zyt528 <zyt528@outlook.com>
2020-11-16 22:50:34 +0800

    add new solution

--- java/Foo.java
+++ java/Foo.java
@@ -1,35 +1,43 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 class Foo {
 
-    private CountDownLatch c1;
-
-    private CountDownLatch c2;
-
-    public Foo() {
-        this.c1 = new CountDownLatch(1);
-        this.c2 = new CountDownLatch(1);
-    }
-
-    public void first() {
-        System.out.print("one");
-        c1.countdown();
-    }
-
-    public void second() {
-        c1.await();
-        System.out.print("two");
-        c2.countdown();
-    }
-
-    public void third() {
-        c2.await();
-        System.out.print("three");
-    }
-
-
     public static void main(String[] args) {
-        Foo foo = new Foo();
-        foo.first();
+        isValid("{}");
     }
 
+    public static boolean isValid(String s) {
+
+        if (s == null || s.length() == 0) {
+            return false;
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('{', -1);
+        map.put('}', 1);
+        map.put('[', -2);
+        map.put(']', 2);
+        map.put('{', -3);
+        map.put('}', 3);
+
+        char[] chars = s.toCharArray();
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(0);
+        for (char c : chars) {
+            int i = map.get(c);
+            if (stack.peek() + i == 0) {
+                stack.pop();
+            } else {
+                stack.push(i);
+            }
+        }
+
+        if (stack.peek() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
